6c89e589216061474aa60503bebf441a1fdfce28
alex <arodriguez202020@gmail.com>
2021-08-17 14:07:44 -0500

    add image to users table and controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     user = User.new(
       name: params[:name],
       email: params[:email],
+      image: params[:image],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
@@ -23,6 +24,7 @@ class UsersController < ApplicationController
     user = User.find_by(id: params[:id])
     user.name = params[:name] || user.name
     user.email = params[:email] || user.email
+    user.image = params[:image] || user.image
     if user.save
       render json: user
     else 
