b8f4e7538e5b3b825c99480ffeee5529f37d276d
alex <arodriguez202020@gmail.com>
2021-08-31 11:16:57 -0500

    update typo in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://
-    gracious-goldwasser-37a1b1.netlify.app'
+    origins 'localhost:8080', 'https://gracious-goldwasser-37a1b1.netlify.app'
 
     resource '*',
       headers: :any,
