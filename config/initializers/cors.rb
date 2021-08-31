c3590d50ae3315696cacdb0b8dc7567c9bb47e99
alex <arodriguez202020@gmail.com>
2021-08-31 11:02:07 -0500

    update cors file to include netlify frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'example.com'
+    origins 'localhost:8080', 'https://
+    gracious-goldwasser-37a1b1.netlify.app'
 
     resource '*',
       headers: :any,
